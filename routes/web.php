6b7b6ff61b499be620137dd8c9c93984fe426ef7
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2025-11-07 23:06:14 -0500

    feat: Agregar middleware de autenticación a la ruta de inicio

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/inicio');
 
 Route::livewire('/inicio', 'pages::inicio.index')
+    ->middleware('auth')
     ->name('inicio.index');
 
 Route::livewire('/login', 'pages::auth.login')
