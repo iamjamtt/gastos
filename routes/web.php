81b863e2cc7a855f6aaaf1a87a94f1fc913c7d05
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2025-11-10 12:57:57 -0500

    feat: Reorganizar rutas de inicio de sesión y controles en el archivo de rutas

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::livewire('/login', 'pages::auth.login')
+    ->middleware('guest')
+    ->name('login');
+
 Route::redirect('/', '/inicio');
 
 Route::livewire('/inicio', 'pages::inicio.index')
     ->middleware('auth')
     ->name('inicio.index');
 
-Route::livewire('/login', 'pages::auth.login')
-    ->middleware('guest')
-    ->name('login');
+Route::livewire('/controles', 'pages::control.index')
+    ->middleware('auth')
+    ->name('control.index');
